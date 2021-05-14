6b20eab2f0e4889c5dd526150095f2088cbfed0c
Yad Smood <y.s.inside@gmail.com>
2021-05-14 10:04:40 +0800

    use coverage.out as the default coverage file name

--- cmd/check-cov/main.go
+++ cmd/check-cov/main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/ysmood/got"
 )
 
-var covFile = flag.String("cov-file", "coverage.txt", "the path of the coverage report")
+var covFile = flag.String("cov-file", "coverage.out", "the path of the coverage report")
 var min = flag.Float64("min", 100, "min coverage rate or exit code with 1")
 
 func main() {
