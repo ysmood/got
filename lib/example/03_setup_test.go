78357e4945aacada87fac91cd5a3feb6c1d92d41
Yad Smood <y.s.inside@gmail.com>
2022-04-01 12:56:59 +0800

    update gotrace

--- lib/example/03_setup_test.go
+++ lib/example/03_setup_test.go
@@ -1,7 +1,6 @@
 package example_test
 
 import (
-	"testing"
 	"time"
 
 	"github.com/ysmood/got"
@@ -13,22 +12,15 @@ func init() {
 	got.DefaultFlags("timeout=10s")
 }
 
-func TestMain(m *testing.M) {
-	// Make sure we don't leaking goroutines
-	gotrace.CheckMain(m, 0)
-}
-
 var setup = got.Setup(func(g got.G) {
 	// The function passed to it will be surely executed after the test
 	g.Cleanup(func() {})
 
-	if got.Parallel() > 0 {
-		// Concurrently run each test
-		g.Parallel()
-	} else {
-		// Make sure there's no goroutine leak for each test
-		gotrace.Check(g, 0)
-	}
+	// Concurrently run each test
+	g.Parallel()
+
+	// Make sure there's no goroutine leak for each test
+	gotrace.CheckLeak(g, 0)
 
 	// Timeout for each test
 	g.PanicAfter(time.Second)
--- lib/example/go.mod
+++ lib/example/go.mod
@@ -4,7 +4,7 @@ go 1.17
 
 require (
 	github.com/ysmood/got v0.0.0-00010101000000-000000000000
-	github.com/ysmood/gotrace v0.3.1
+	github.com/ysmood/gotrace v0.5.1
 )
 
 replace github.com/ysmood/got => ../../
