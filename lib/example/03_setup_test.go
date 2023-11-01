8f3e3456b9ca8795d516a1616d642dc7263c7dbf
Yad Smood <y.s.inside@gmail.com>
2023-11-02 03:21:46 +0800

    doc

--- lib/example/03_setup_test.go
+++ lib/example/03_setup_test.go
@@ -1,6 +1,7 @@
 package example_test
 
 import (
+	"testing"
 	"time"
 
 	"github.com/ysmood/got"
@@ -12,7 +13,18 @@ func init() {
 	got.DefaultFlags("timeout=10s")
 }
 
-var setup = got.Setup(func(g got.G) {
+// G is your custom test context.
+type G struct {
+	got.G
+
+	// You can add your own fields, usually data you want init before each test.
+	time string
+}
+
+// setup is a helper function to setup your test context G.
+var setup = func(t *testing.T) G {
+	g := got.T(t)
+
 	// The function passed to it will be surely executed after the test
 	g.Cleanup(func() {})
 
@@ -24,4 +36,13 @@ var setup = got.Setup(func(g got.G) {
 
 	// Timeout for each test
 	g.PanicAfter(time.Second)
-})
+
+	return G{g, time.Now().Format(time.DateTime)}
+}
+
+func TestSetup(t *testing.T) {
+	g := setup(t)
+
+	// Here we use the custom field we have defined in G
+	g.Gt(g.time, "2023-01-02 15:04:05")
+}
